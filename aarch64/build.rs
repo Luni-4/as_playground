cc5237cb6dee3cce43e7b91e46ea25f8b36ac9d5
Luni-4 <luni-4@hotmail.it>
2020-01-29 19:11:12 +0100

    Cross-compile using aarch64 target

--- aarch64/build.rs
+++ aarch64/build.rs
@@ -1,8 +1,5 @@
-#![feature(rustc_private)]
 extern crate cc;
 
 fn main() {
-    cc::Build::new()
-        .file("src/add.s")
-        .compile("my-asm-lib");
+    cc::Build::new().file("src/add.s").compile("my-asm-lib");
 }
